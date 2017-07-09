e52d335065f639b69eb3635d1e8baa6efc3f53bc
Himiko <kozue3456@hotmail.com>
2017-07-09 15:04:51 -0700

    making revisions to my C socket program serverside

--- socketplay_server.c
+++ socketplay_server.c
@@ -8,21 +8,36 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <strings.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	int server_fd=0;
 	int new_socket=0;
 	int valread=0;
+	int set_socketopt=0;
+	int iPort=-1;
+	struct sockaddr_in servaddr, cliaddr; 
+	int clilen; 
+	int n; 
+	
+	char sendBuff[256];
+	if(argc<2)
+	{
+		fprintf(stderr,"There is no port number provided please try again with a port number\n");
+		exit(EXIT_FAILURE);
+	}
 	
 	server_fd=socket(AF_INET,SOCK_STREAM,0);
-	if(server_fd == 0)
+	if(server_fd <0)
 	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
 	
-	
+	memset(&servaddr,'0',sizeof(servaddr));
+	memset(&sendBuff,'0',sizeof(sendBuff));
+	iPort=atoi(argv[1]);
 	
 	
 		
