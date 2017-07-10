e048de796e3f44dba7f433a568953676a1a535db
Himiko <kozue3456@hotmail.com>
2017-07-09 17:50:55 -0700

    adding more changes to my server program

--- socketplay_server.c
+++ socketplay_server.c
@@ -15,10 +15,13 @@ int main(int argc, char *argv[])
 	int server_fd=0;
 	int new_socket=0;
 	int valread=0;
-	int set_socketopt=0;
+	int iInterfaceassoc=-1;
+	int set_socketopt=1;
+	
 	int iPort=-1;
 	struct sockaddr_in servaddr, cliaddr; 
-	int clilen; 
+	int clilen=-1; // length of client ip address 
+	int iListenSuccess=-1;
 	int n; 
 	
 	char sendBuff[256];
@@ -35,6 +38,12 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&set_socketopt, sizeof(set_socketopt)))
+    {
+        perror("Failed to set the appropiate socket options");
+        exit(EXIT_FAILURE);
+    }
+	
 	memset(&servaddr,'0',sizeof(servaddr));
 	memset(&sendBuff,'0',sizeof(sendBuff));
 	iPort=atoi(argv[1]);
@@ -42,6 +51,26 @@ int main(int argc, char *argv[])
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(iPort);
-    
-		
+    iInterfaceassoc=bind(server_fd,(struct sockaddr *) &servaddr,sizeof(servaddr));
+    if(iInterfaceassoc<0)
+    {
+		perror("Failed to associate socket with network interface");
+		exit(EXIT_FAILURE);
+	}
+	iListenSuccess=listen(server_fd,32);
+	if(iListenSuccess<0)
+	{
+		perror("Listening failed, try again later");
+		exit(EXIT_FAILURE);
+	}
+	clilen=sizeof(cliaddr);
+	/*
+	 * Just an aside to myself as the developer: 
+	 * 	 The accept system call does the following:
+	 * 			Takes the first pending connection from the queue for the listening
+	 * 			socket [which in our case is the socket associated with the file descriptor server_fd]
+	 * creates a new socket file descriptor referring to that socket. At that point, client and server are ready to exchange data. 
+	 * */
+	new_socket=accept(server_fd,(struct *sockaddr) &cliaddr, &clilen);
+	
 }
