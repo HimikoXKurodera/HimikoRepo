6bb0d65cd528256199e527a2576c3f798cc297f9
Himiko <kozue3456@hotmail.com>
2017-07-10 17:49:35 -0700

    TO DO: Fix segfault and /usr/bin/ld: client: stderr: invalid version 2 (max 0) client: error adding symbols: Bad value collect2: error: ld returned 1 exit status

--- socketplay_client.c
+++ socketplay_client.c
@@ -17,10 +17,11 @@ int main(int argc, char const *argv[])
 	int iPort=-1;
 	int iMiddleman=-1;
 	int iClientLen=-1;
+	int iConnectSucess=-1;
 	char userbuff[256];
 	
 	struct sockaddr_in servaddr;
-	struct hostent server;
+	struct hostent *server;
 	if(argc<3)
 	{
 		fprintf(stderr,"Less than three arguments passed, the usage for this client program is %s hostname portname\n", argv[0]);
@@ -33,13 +34,37 @@ int main(int argc, char const *argv[])
 		perror("Error opening socket");
 		exit(0);
 	}
-	iServeraddr=gethostbyname(argv[1]);
-	if(iServeraddr==NULL)
+	server=gethostbyname(argv[1]);
+	if(server==NULL)
 	{
 		fprintf(stderr,"Error no such host\n");
 		exit(0);
 	}
 	memset(&servaddr,'0',sizeof(servaddr));
 	servaddr.sin_family=AF_INET;
-	
+	bcopy((char *)server->h_addr,(char *)servaddr.sin_addr.s_addr,server->h_length);
+	servaddr.sin_port=htons(iPort);
+	iConnectSucess=connect(iClientfd,&servaddr,sizeof(servaddr));
+	if(iConnectSucess<0)
+	{
+		perror("Connection to server failed\n");
+		exit(0);
+	}
+	printf("Please enter your message: \n");
+	bzero(userbuff,256);
+	scanf("%s",userbuff);
+	iMiddleman=write(iClientfd,userbuff,255);
+	if(iMiddleman<0)
+	{
+		perror("Failed to write to socket exiting...");
+		exit(0);
+	}
+	iMiddleman=read(iClientfd,userbuff,255);
+	if(iMiddleman<0)
+	{
+		perror("Failed to read to socket exiting...");
+		exit(0);
+    }
+    printf("%s/n",userbuff);
+    return 0;
 }
